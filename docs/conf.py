6bc3ebd329588fb0db62ed195529b6c7e6ca34a5
ryandaepark <ryan@moonbeam.ai>
2022-03-29 11:59:54 -0700

    Updated version.

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Moonbeam'
 author = 'Moonbeam'
 
 # The full version, including alpha/beta/rc tags
-release = '1.14'
+release = '1.18'
 
 
 # -- General configuration ---------------------------------------------------
